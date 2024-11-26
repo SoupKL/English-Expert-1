363f6b1726ac843aeb4c57105968792696a60778
valia <valia0119@outlook.com>
2024-11-26 18:53:09 +1000

    Страница прифиля

---
+++ English-Expert/src/components/courses-block/courses-block.vue
@@ -0,0 +1,43 @@
+<script>
+
+</script>
+
+<template>
+
+<div class="courses">
+  <div class="courses_block">
+	<div class="courses_programs">
+	  <a>Программа</a>
+	</div>
+	<div class="courses_level">
+		<p>Уровень С1</p>
+	</div>
+	<div class="courses_text">
+	  <p>Совершенствуйте свой язык и обретайте уверенность в общении на высоком уровне. Курс для тех, кто стремится к языковому мастерству.</p>
+	</div>
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+.courses{
+  background: rgb(221, 223, 230);
+  display:        flex;
+  flex-direction: column;
+  align-items:    center;
+  width:          500px;
+  min-height:     380px;
+  border-radius:  20px;
+}
+.courses_block{
+  width: 402px;
+  height: 250px;
+  margin-top: 38px;
+  border-radius: 20px;
+  background: rgb(184, 239, 207);
+  padding: 28px;
+}
+
+
+</style>
